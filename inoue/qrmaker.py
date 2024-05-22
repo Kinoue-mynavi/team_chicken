1dcb48221913103bef762e97e0e06d22d8fa502a
EC2 Default User <ec2-user@ip-172-31-42-14.ap-northeast-1.compute.internal>
2024-05-22 07:35:39 +0000

    refactor: modify qrmaker.py

--- inoue/qrmaker.py
+++ inoue/qrmaker.py
@@ -1,15 +1,20 @@
 import qrcode
 import os
 
-def read_file_at_dir(dir_path, filename):
+# ターゲットとなるパスを生成
+def create_file_path(dir_path, file_name):
+    return os.path.join(dir_path, file_name)
+
+# 任意のファイルを読み込む
+def read_file_at_dir(dir_path, file_name):
     os.makedirs(dir_path, exist_ok=True)
-    f = open(os.path.join(dir_path, filename), "r")
+    f = open(create_file_path(dir_path, file_name), "r")
     lines = f.readlines()
     f.close()
 
-    # 改行を取り除いて変換
-    return [line.rstrip("\n") for line in lines]
+    return [line.rstrip("\n") for line in lines] # 改行を取り除いた上で返却
 
+# QRコードを生成
 def generate_qr_code(url, file_path):
     qr = qrcode.QRCode(
         version=1,
@@ -24,16 +29,11 @@ def generate_qr_code(url, file_path):
     img = qr.make_image(fill_color="black", back_color="white")
     img.save(file_path)
     img.show()
-
-OUTPUT_DIRECTORY = "./inoue/output"
-INPUT_DIRECTORY = "./inoue"
-
-def create_file(file_name):
-    return os.path.join(OUTPUT_DIRECTORY, f"{file_name}.png")
         
 if __name__ == "__main__":
-    urls = read_file_at_dir(INPUT_DIRECTORY, "urls.txt")
+    # Reading
+    urls = read_file_at_dir("./inoue", "urls.txt")
 
+    # Writing
     for i, url in enumerate(urls):
-        file_path = create_file(f"{i + 1}")
-        generate_qr_code(url, file_path)
+        generate_qr_code(url, create_file_path("./inoue/output", f"{i + 1}.png"))
